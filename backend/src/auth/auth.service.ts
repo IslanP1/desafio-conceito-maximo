9f74bfb1a164eaa832cc6459214569cca9a4a7b7
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-06-04 10:08:44 -0300

    feat: adicionando dto de login de usuários

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
