2fadfae9f5aaf3c26a5ebb9b73a47e2f2d2718d0
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-06-03 23:02:17 -0300

    fix: corrigindo o bug do output e removendo migrations antigas

--- backend/prisma/migrations/20250604004023_create_user/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "User_cpf_key";
-
--- DropIndex
-DROP INDEX "User_email_key";
--- backend/prisma/migrations/20250604015209_init/migration.sql
+++ backend/prisma/migrations/20250604015209_init/migration.sql
@@ -3,8 +3,6 @@ CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
     "password" TEXT NOT NULL
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
