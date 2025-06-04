084951baced9ebd322e7d97437c85fe23ce64423
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-06-04 10:31:46 -0300

    feat: adicionando serviço para login de usuários utilizando o token jwt

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,12 +2,17 @@
 
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async createUser(dto: CreateUserDto) {
     const hashedPassword = await bcrypt.hash(dto.password, 10);
@@ -25,6 +30,30 @@ export class AuthService {
     return {
       message: 'User created successfully',
       user: result,
+    };
+  }
+
+  async loginUser(dto: LoginUserDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (!user) {
+      return new Error('User not found');
+    }
+
+    const passwordValid = await bcrypt.compare(dto.password, user.password);
+    if (!passwordValid) {
+      return new Error('Invalid password');
+    }
+
+    const payload = { sub: user.id, email: user.email };
+    const token = this.jwtService.sign(payload);
+
+    return {
+      message: 'Login successful',
+      user: { id: user.id, name: user.name, email: user.email },
+      token,
     };
   }
 }
