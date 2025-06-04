ec618bc17573f3811fd9231c36d3e9052091f36c
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-06-03 21:45:22 -0300

    feat: criando o dto de create-user e alterando a entidade user

---
+++ backend/src/auth/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  cpf: string;
+
+  @MinLength(6)
+  password: string;
+}
