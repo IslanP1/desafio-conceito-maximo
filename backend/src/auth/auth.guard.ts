09421b44fa55d6fba20ce13e8218b6816418be8a
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-06-04 11:35:38 -0300

    feat: adicionando um auth guard para controle do admin

---
+++ backend/src/auth/auth.guard.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    if (user?.role !== 'admin') {
+      throw new ForbiddenException('Access denied. Admins only.');
+    }
+    return true;
+  }
+}
