7c190c447a0c655dd68190bdf25bee6b2a73b005
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-06-03 22:18:45 -0300

    config: configurando o cliente prisma e adicionando module e service do prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService, AuthService],
+  providers: [AppService, AuthService, PrismaService],
 })
 export class AppModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,15 @@
+/* eslint-disable @typescript-eslint/require-await */
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async createUser(dto: CreateUserDto) {
+    // Here you would typically hash the password and save the user to the database
+    // For demonstration purposes, we'll just return the DTO
+    return {
+      message: 'User created successfully',
+      user: dto,
+    };
+  }
+}
