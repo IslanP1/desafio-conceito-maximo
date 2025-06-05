e108b075877689e4b96bc134fcd4d3041e52520d
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-06-05 00:08:38 -0300

    feat: adicionando estrtégia jwt

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Module({
   imports: [
@@ -12,6 +13,6 @@ import { JwtModule } from '@nestjs/jwt';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService, PrismaService, JwtStrategy],
 })
 export class AuthModule {}
