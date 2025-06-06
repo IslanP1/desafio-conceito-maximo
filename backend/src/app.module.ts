32bd4905e8c60bd43f126ce48f0bcb8e264fbd0d
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-06-05 23:21:21 -0300

    feat: adicionando dto para criar solicitação

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { SolicitationService } from './solicitation/solicitation.service';
 
 @Module({
   imports: [AuthModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SolicitationService],
 })
 export class AppModule {}
