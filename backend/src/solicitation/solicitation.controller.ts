3860a73da094830799893710ecae286e497d56ab
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-06-06 00:50:27 -0300

    feat: adicionando controller para atualizar status

--- backend/src/solicitation/solicitation.controller.ts
+++ backend/src/solicitation/solicitation.controller.ts
@@ -1,11 +1,14 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { SolicitationService } from './solicitation.service';
 import { JwtGuard } from 'src/auth/jwt/jwt.guard';
 import { CreateSolicitationDto } from './dto/create-solicitation.dto';
+import { UpdateSolicitationStatusDto } from './dto/update-solicitation.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('solicitation')
 export class SolicitationController {
@@ -16,5 +19,11 @@ export class SolicitationController {
   async create(@Body() dto: CreateSolicitationDto, @Req() req) {
     const userId = req.user.userId;
     return this.soclicitationService.create(dto, userId);
+  }
+  
+  @UseGuards(JwtGuard, AuthGuard)
+  @Patch(':id/status')
+  async updateStatus(@Param('id') id: number, @Body() dto: UpdateSolicitationStatusDto) {
+    return this.soclicitationService.updateStatus(Number(id), dto);
   }
 }
