c6ace090090b5f561018350a5eba8d9cc1ca4b18
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-06-05 23:51:22 -0300

    feat: adicionando service e método create de solicitação

--- backend/src/solicitation/solicitation.service.ts
+++ backend/src/solicitation/solicitation.service.ts
@@ -1,4 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateSolicitationDto } from './dto/create-solicitation.dto';
 
 @Injectable()
-export class SolicitationService {}
+export class SolicitationService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(dto: CreateSolicitationDto, userId: number) {
+    const user = await this.prismaService.user.findUnique({
+      where: { id: userId },
+    });
+
+    return this.prismaService.solicitacao.create({
+      data: {
+        tipoSolicitacao: dto.tipoSolicitacao,
+        endereco: dto.endereco,
+        descricao: dto.descricao,
+        nomeSolicitante: user?.name ?? '',
+        cpfSolicitante: user?.cpf ?? '',
+      },
+    });
+  }
+}
