31189ea37dc6ca4b6aadc30767ab183f28f53583
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-06-06 21:57:01 -0300

    feat: ajustes versão inicial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,16 +5,20 @@ import Home from './pages/Home';
 import Solicitation from './pages/Solicitation';
 import SolicitationList from './pages/SolicitationRead';
 import PainelAdmin from './pages/PainelAdmin';
+import Menu from './components/Menu';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/solicitation" element={<Solicitation />} />
-      <Route path="/solicitation/list" element={<SolicitationList />} />
-      <Route path="/admin" element={<PainelAdmin />} />
-    </Routes>
+    <>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/solicitation" element={<Solicitation />} />
+        <Route path="/solicitation/list" element={<SolicitationList />} />
+        <Route path="/admin" element={<PainelAdmin />} />
+      </Routes>
+    </>
   );
 }
